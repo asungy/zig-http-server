2821697dab2a077d36b9e72bd1e619d50b0b1705
asungy <62207329+asungy@users.noreply.github.com>
2024-07-19 16:11:00 -0400

    Replaced readAll with read method

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const net = std.net;
+const print = std.debug.print;
 
 fn get_url(request: []u8) ?[]const u8 {
     var it = std.mem.split(u8, request, " ");
@@ -8,8 +9,6 @@ fn get_url(request: []u8) ?[]const u8 {
 }
 
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
-
     const addr = "127.0.0.1";
     const port = 4221;
     const address = try net.Address.resolveIp(addr, port);
@@ -17,18 +16,18 @@ pub fn main() !void {
         .reuse_address = true,
     });
     defer listener.deinit();
-    try stdout.print("Listening on {s}:{d}\n", .{addr, port});
+    print("Listening on {s}:{d}\n", .{addr, port});
 
     var conn = try listener.accept();
+    defer conn.stream.close();
+
     var buffer: [1024]u8 = undefined;
-    _ = try conn.stream.reader().readAll(&buffer);
+    _ = try conn.stream.reader().read(&buffer);
 
     const url = get_url(&buffer).?;
     if (std.mem.eql(u8, url, "/")) {
-        try stdout.print("OK", .{});
         try conn.stream.writer().writeAll("HTTP/1.1 200 OK\r\n\r\n");
     } else {
-        try stdout.print("Not Found", .{});
         try conn.stream.writer().writeAll("HTTP/1.1 404 Not Found\r\n\r\n");
     }
 }
