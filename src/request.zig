f469b33c9e849927872c1d8e5d83b2ec70ac6bc0
asungy <62207329+asungy@users.noreply.github.com>
2024-07-20 13:25:12 -0400

    Added target and version to Request struct.

--- src/request.zig
+++ src/request.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 const Method = enum {
     GET,
 
-    fn matchString(str: []const u8) ?Method {
+    fn fromString(str: []const u8) ?Method {
         return if (std.mem.eql(u8, str, "GET")) {
             return Method.GET;
         } else {
@@ -12,12 +12,28 @@ const Method = enum {
     }
 };
 
+const HttpVersion = enum {
+    Http_1_1,
+
+    fn fromString(str: []const u8) ?HttpVersion {
+        return if (std.mem.eql(u8, str, "HTTP/1.1")) {
+            return HttpVersion.Http_1_1;
+        } else {
+            return null;
+        };
+    }
+};
+
 const ParseError = error {
-    UnknownMethod,
+    NoMethod,
+    NoTarget,
+    NoHttpVersion,
 };
 
 const Request = struct {
     method: Method,
+    target: []const u8,
+    version: HttpVersion,
 
     pub fn parse(raw_request: []const u8) ParseError!Request {
         var lines = std.mem.split(u8, raw_request, "\r\n");
@@ -25,16 +41,22 @@ const Request = struct {
         var start_line_iter = std.mem.split(u8, start_line, " ");
 
         var method: ?Method = null;
+        var target: ?[]const u8 = null;
+        var version: ?HttpVersion = null;
         for (0..3) |i| {
             const s = start_line_iter.next() orelse break;
             switch (i) {
-                0 => method = Method.matchString(s),
+                0 => method = Method.fromString(s),
+                1 => target = s,
+                2 => version = HttpVersion.fromString(s),
                 else => break,
             }
         }
 
         return Request {
-            .method = method orelse return ParseError.UnknownMethod,
+            .method = method orelse return ParseError.NoMethod,
+            .target = target orelse return ParseError.NoTarget,
+            .version = version orelse return ParseError.NoHttpVersion,
         };
     }
 };
@@ -42,4 +64,5 @@ const Request = struct {
 test "http request parses into Request struct" {
     const request = try Request.parse("GET /echo/abc HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: curl/7.64.1\r\nAccept: */*\r\n\r\n");
     try std.testing.expect(request.method == Method.GET);
+    try std.testing.expect(std.mem.eql(u8, request.target, "/echo/abc"));
 }
