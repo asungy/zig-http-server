0b95156904729a108656ad1d4a42c44eec9f7980
asungy <62207329+asungy@users.noreply.github.com>
2024-07-24 23:13:36 -0400

    Implemented status line bytes

--- src/http/response.zig
+++ src/http/response.zig
@@ -42,7 +42,7 @@ pub const Response = struct {
 
     pub fn setContentLength(self: *Response, size: usize) !void {
         const allocator = self.arena.allocator();
-        var length: u16 = 0;
+        var length: usize = 0;
         var _size = size;
         while (_size > 0) {
             _size /= 10;
@@ -61,42 +61,77 @@ pub const Response = struct {
         try self.setContentLength(@intCast(body.len));
     }
 
-    // pub fn toBytes(self: *Response, allocator: std.mem.Allocator) ![]const u8 {
-    //
-    // }
-    //
-    // fn statusLineBytes(self: *Response, allocator: std.mem.Allocator) ![]const u8 {
-    //     // HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\nabc
-    //     // version
-    //     // status code
-    //     // status
-    //     // CRLF
-    //     var version = self.version.toString();
-    //     var status = self.status.toString();
-    // }
+    pub fn toBytes(self: *Response, allocator: std.mem.Allocator) ![]const u8 {
+        const statusLine = try self.statusLineBytes(allocator);
+        defer allocator.free(statusLine);
+
+        const buffer = try allocator.alloc(u8, statusLine.len);
+        std.mem.copyForwards(u8, buffer, statusLine);
+
+        return buffer;
+    }
+
+    fn statusLineBytes(self: *Response, allocator: std.mem.Allocator) ![]const u8 {
+        // Calculating size of status line.
+        const space = " ";
+        const crlf = "\r\n";
+        const version = self.version.toString();
+        var statusCode: [3]u8 = undefined;
+        _ = std.fmt.formatIntBuf(&statusCode, self.status.code(), 10, .lower, .{});
+        const status = self.status.toString();
+        const size = version.len + space.len
+            + statusCode.len + space.len
+            + status.len + crlf.len;
+
+        var counter: usize = 0;
+        const buffer = try allocator.alloc(u8, size);
+
+        // Copying HTTP version.
+        std.mem.copyForwards(u8, buffer[counter..], version);
+        counter += version.len;
+        std.mem.copyForwards(u8, buffer[counter..], space);
+        counter += space.len;
+
+        // Copying status code.
+        std.mem.copyForwards(u8, buffer[counter..], &statusCode);
+        counter += statusCode.len;
+        std.mem.copyForwards(u8, buffer[counter..], space);
+        counter += space.len;
+
+        // Copying status.
+        std.mem.copyForwards(u8, buffer[counter..], status);
+        counter += status.len;
+        std.mem.copyForwards(u8, buffer[counter..], crlf);
+
+        return buffer;
+    }
     //
     // fn headersBytes(self: *Response, allocator: std.mem.Allocator) ![]const u8 {
     //
     // }
 };
 
-// test "Response struct to bytes" {
-//     var response = try Response.init(std.testing.allocator);
-//     defer response.deinit();
-//
-//     try response.setContentType(http.ContentType.TextPlain);
-//     const contentType = response.headers.get("Content-Type").?;
-//     try std.testing.expect(std.mem.eql(u8, contentType, http.ContentType.TextPlain.toString()));
-//
-//     try response.setBody("Hello World");
-//     try std.testing.expect(std.mem.eql(u8, response.body.?, "Hello World"));
-//
-//     const contentLength = response.headers.get("Content-Length").?;
-//     try std.testing.expect(std.mem.eql(u8, contentLength, "11"));
-//
-//     const bytes = try response.toBytes(std.testing.allocator);
-//     try std.testing.expect(std.mem.eql(u8, bytes, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 11\r\n\r\nHello World"));
-// }
+test "Response struct to bytes" {
+    var response = try Response.init(std.testing.allocator);
+    defer response.deinit();
+
+    try response.setContentType(http.ContentType.TextPlain);
+    const contentType = response.headers.get("Content-Type").?;
+    try std.testing.expect(std.mem.eql(u8, contentType, http.ContentType.TextPlain.toString()));
+
+    try response.setBody("Hello World");
+    try std.testing.expect(std.mem.eql(u8, response.body.?, "Hello World"));
+
+    const contentLength = response.headers.get("Content-Length").?;
+    try std.testing.expect(std.mem.eql(u8, contentLength, "11"));
+
+    const bytes = try response.toBytes(std.testing.allocator);
+    defer std.testing.allocator.free(bytes);
+
+    std.log.warn("{s}", .{bytes});
+    // try std.testing.expect(std.mem.eql(u8, bytes, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 11\r\n\r\nHello World"));
+    try std.testing.expect(std.mem.eql(u8, bytes, "HTTP/1.1 200 OK\r\n"));
+}
 
 test "setting content length" {
     var response = try Response.init(std.testing.allocator);
