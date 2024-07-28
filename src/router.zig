1e8328796759e18fadf1d23d3c8d9b7d956a2d42
asungy <62207329+asungy@users.noreply.github.com>
2024-07-28 16:20:10 -0400

    Added more robust testing

--- src/router.zig
+++ src/router.zig
@@ -14,7 +14,10 @@ const RouteTrie = struct {
         children: std.StringHashMap(*Node),
         handler: ?RouteHandler,
 
-        const delim = "/";
+        const delim = '/';
+        inline fn delimString() []const u8 {
+            return comptime &[1]u8{delim};
+        }
 
         const Kind = enum {
             Static,
@@ -34,11 +37,13 @@ const RouteTrie = struct {
             self.children.deinit();
         }
 
-        fn findNextMatchingNode(self: *Node, target: []const u8) *Node {
-            std.debug.assert(std.mem.eql(u8, self.key, Node.delim));
-            std.debug.assert(target.len > 0);
+        fn findMatchingNode(self: *Node, target: []const u8) ?*Node {
+            if (target.len == 0 or target[0] != Node.delim or
+                !std.mem.eql(u8, self.key, Node.delimString())) return null;
 
-            var paths = std.mem.split(u8, target, Node.delim);
+            if (std.mem.eql(u8, target, Node.delimString())) return self;
+
+            var paths = std.mem.split(u8, target, Node.delimString());
             var prev_node = self;
             var current_node = self;
             var current_path = paths.next();
@@ -49,21 +54,18 @@ const RouteTrie = struct {
                     current_node = next_node;
                     current_path = next_path;
                 } else {
-                    return prev_node;
+                    return null;
                 }
             }
-
 
             return current_node;
         }
     };
 };
 
-
-
-test "find next matching (static) node" {
+test "find matching static node" {
     const Node = RouteTrie.Node;
-    var rootNode = Node.init(Node.delim, Node.Kind.Static, null, std.testing.allocator); defer rootNode.deinit();
+    var rootNode = Node.init(Node.delimString(), Node.Kind.Static, null, std.testing.allocator); defer rootNode.deinit();
     var node1    = Node.init("abc", Node.Kind.Static, null, std.testing.allocator); defer node1.deinit();
     var node2    = Node.init("def", Node.Kind.Static, null, std.testing.allocator); defer node2.deinit();
     var node3    = Node.init("ghi", Node.Kind.Static, null, std.testing.allocator); defer node3.deinit();
@@ -72,8 +74,18 @@ test "find next matching (static) node" {
     try node1.children.put(node2.key, &node2);
     try node2.children.put(node3.key, &node3);
 
-    try std.testing.expectEqual(&rootNode, rootNode.findNextMatchingNode("/"));
-    try std.testing.expectEqual(&node1,    rootNode.findNextMatchingNode("/abc"));
-    try std.testing.expectEqual(&node2,    rootNode.findNextMatchingNode("/abc/def"));
-    try std.testing.expectEqual(&node3,    rootNode.findNextMatchingNode("/abc/def/ghi"));
+    // Error checking.
+    try std.testing.expectEqual(null, rootNode.findMatchingNode(""));
+    try std.testing.expectEqual(null, rootNode.findMatchingNode("abc"));
+    try std.testing.expectEqual(null, node1.findMatchingNode("abc"));
+
+    // Positive case.
+    try std.testing.expectEqual(&rootNode, rootNode.findMatchingNode("/").?);
+    try std.testing.expectEqual(&node1,    rootNode.findMatchingNode("/abc").?);
+    try std.testing.expectEqual(&node2,    rootNode.findMatchingNode("/abc/def").?);
+    try std.testing.expectEqual(&node3,    rootNode.findMatchingNode("/abc/def/ghi").?);
+
+    // Negative case.
+    try std.testing.expectEqual(null, rootNode.findMatchingNode("/xyz"));
+    try std.testing.expectEqual(null, rootNode.findMatchingNode("/abc/def/ghi/jkl"));
 }
