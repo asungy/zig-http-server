0a08dc6a2b9943d58a9656209d136d8d531704db
asungy <62207329+asungy@users.noreply.github.com>
2024-07-30 22:26:01 -0400

    Refactored files to use @This.

--- src/http/request.zig
+++ src/http/request.zig
@@ -1,5 +1,10 @@
 const std = @import("std");
 const http = @import("http.zig");
+pub const Request = @This();
+
+method: http.Method,
+target: []const u8,
+version: http.Version,
 
 pub const ParseError = error {
     NoMethod,
@@ -7,36 +12,30 @@ pub const ParseError = error {
     NoHttpVersion,
 };
 
-pub const Request = struct {
-    method: http.Method,
-    target: []const u8,
-    version: http.Version,
+pub fn parse(raw_request: []const u8) ParseError!Request {
+    var lines = std.mem.split(u8, raw_request, "\r\n");
+    const start_line = lines.next().?;
+    var start_line_iter = std.mem.split(u8, start_line, " ");
 
-    pub fn parse(raw_request: []const u8) ParseError!Request {
-        var lines = std.mem.split(u8, raw_request, "\r\n");
-        const start_line = lines.next().?;
-        var start_line_iter = std.mem.split(u8, start_line, " ");
-
-        var method: ?http.Method = null;
-        var target: ?[]const u8 = null;
-        var version: ?http.Version = null;
-        for (0..3) |i| {
-            const s = start_line_iter.next() orelse break;
-            switch (i) {
-                0 => method = http.Method.fromString(s),
-                1 => target = s,
-                2 => version = http.Version.fromString(s),
-                else => break,
-            }
+    var method: ?http.Method = null;
+    var target: ?[]const u8 = null;
+    var version: ?http.Version = null;
+    for (0..3) |i| {
+        const s = start_line_iter.next() orelse break;
+        switch (i) {
+            0 => method = http.Method.fromString(s),
+            1 => target = s,
+            2 => version = http.Version.fromString(s),
+            else => break,
         }
-
-        return Request {
-            .method = method orelse return ParseError.NoMethod,
-            .target = target orelse return ParseError.NoTarget,
-            .version = version orelse return ParseError.NoHttpVersion,
-        };
     }
-};
+
+    return Request {
+        .method = method orelse return ParseError.NoMethod,
+        .target = target orelse return ParseError.NoTarget,
+        .version = version orelse return ParseError.NoHttpVersion,
+    };
+}
 
 test "http request parses into Request struct" {
     const request = try Request.parse("GET /echo/abc HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: curl/7.64.1\r\nAccept: */*\r\n\r\n");
--- src/http/response.zig
+++ src/http/response.zig
@@ -1,156 +1,155 @@
 const std = @import("std");
 const http = @import("http.zig");
 const Allocator = std.mem.Allocator;
+pub const Response = @This();
 
-pub const Response = struct {
-    arena: std.heap.ArenaAllocator,
-    version: http.Version,
-    status: http.Status,
-    headers: std.StringHashMap([]const u8),
-    body: ?[]const u8,
+arena: std.heap.ArenaAllocator,
+version: http.Version,
+status: http.Status,
+headers: std.StringHashMap([]const u8),
+body: ?[]const u8,
 
-    pub fn init(allocator: std.mem.Allocator) !Response {
-        const arena = std.heap.ArenaAllocator.init(allocator);
-        return Response {
-            .arena = arena,
-            .version = http.Version.Http_1_1,
-            .status  = http.Status.OK,
-            .headers = std.StringHashMap([]const u8).init(allocator),
-            .body = null,
-        };
+pub fn init(allocator: std.mem.Allocator) !Response {
+    const arena = std.heap.ArenaAllocator.init(allocator);
+    return Response {
+        .arena = arena,
+        .version = http.Version.Http_1_1,
+        .status  = http.Status.OK,
+        .headers = std.StringHashMap([]const u8).init(allocator),
+        .body = null,
+    };
+}
+
+pub fn deinit(self: *Response) void {
+    _ = self.arena.reset(.free_all);
+    self.headers.deinit();
+}
+
+pub fn setStatus(self: *Response, status: http.Status) void {
+    self.status = status;
+}
+
+pub fn addHeader(self: *Response, key: []const u8, value: []const u8) !void {
+    const allocator = self.arena.allocator();
+    const buffer = try allocator.alloc(u8, value.len);
+    std.mem.copyForwards(u8, buffer, value);
+    try self.headers.put(key, buffer);
+}
+
+pub fn setContentType(self: *Response, content_type: http.ContentType) !void {
+    try self.addHeader("Content-Type", content_type.toString());
+}
+
+pub fn setContentLength(self: *Response, size: usize) !void {
+    const allocator = self.arena.allocator();
+    var length: usize = 0;
+    var _size = size;
+    while (_size > 0) {
+        _size /= 10;
+        length += 1;
+    }
+    const buffer = try allocator.alloc(u8, length);
+    _ = std.fmt.formatIntBuf(buffer, size, 10, .lower, .{});
+    try self.addHeader("Content-Length", buffer);
+}
+
+pub fn setBody(self: *Response, body: []const u8) !void {
+    const allocator = self.arena.allocator();
+    const buffer = try allocator.alloc(u8, body.len);
+    std.mem.copyForwards(u8, buffer, body);
+    self.body = buffer;
+    try self.setContentLength(@intCast(body.len));
+}
+
+pub fn serialize(self: *Response, allocator: std.mem.Allocator) ![]const u8 {
+    const statusLine = try self.statusLineBytes(allocator);
+    defer allocator.free(statusLine);
+
+    const headers = try self.headersBytes(allocator);
+    defer allocator.free(headers);
+
+    const bodyLength = if (self.body) |body| body.len else 0;
+    const buffer = try allocator.alloc(u8, statusLine.len + headers.len + bodyLength);
+    var counter: usize = 0;
+    std.mem.copyForwards(u8, buffer, statusLine);
+    counter += statusLine.len;
+    std.mem.copyForwards(u8, buffer[counter..], headers);
+    counter += headers.len;
+    if (bodyLength > 0) {
+        std.mem.copyForwards(u8, buffer[counter..], self.body.?);
     }
 
-    pub fn deinit(self: *Response) void {
-        _ = self.arena.reset(.free_all);
-        self.headers.deinit();
+    return buffer;
+}
+
+fn statusLineBytes(self: *Response, allocator: std.mem.Allocator) ![]const u8 {
+    // Calculating size of status line.
+    const space = " ";
+    const crlf = "\r\n";
+    const version = self.version.toString();
+    var statusCode: [3]u8 = undefined;
+    _ = std.fmt.formatIntBuf(&statusCode, self.status.code(), 10, .lower, .{});
+    const status = self.status.toString();
+    const size = version.len + space.len
+        + statusCode.len + space.len
+        + status.len + crlf.len;
+
+    var counter: usize = 0;
+    const buffer = try allocator.alloc(u8, size);
+
+    // Copying HTTP version.
+    std.mem.copyForwards(u8, buffer[counter..], version);
+    counter += version.len;
+    std.mem.copyForwards(u8, buffer[counter..], space);
+    counter += space.len;
+
+    // Copying status code.
+    std.mem.copyForwards(u8, buffer[counter..], &statusCode);
+    counter += statusCode.len;
+    std.mem.copyForwards(u8, buffer[counter..], space);
+    counter += space.len;
+
+    // Copying status.
+    std.mem.copyForwards(u8, buffer[counter..], status);
+    counter += status.len;
+    std.mem.copyForwards(u8, buffer[counter..], crlf);
+
+    return buffer;
+}
+
+fn headersBytes(self: *Response, allocator: std.mem.Allocator) ![]const u8 {
+    var it = self.headers.iterator();
+    var size: usize = 0;
+    const delim = ": ";
+    const crlf = "\r\n";
+    // Calculating buffer size.
+    while (it.next()) |kv| {
+         size += kv.key_ptr.len + delim.len + kv.value_ptr.len + crlf.len;
     }
+    size += crlf.len;
+    const buffer = try allocator.alloc(u8, size);
 
-    pub fn setStatus(self: *Response, status: http.Status) void {
-        self.status = status;
-    }
+    // Copying headers to buffer.
+    it = self.headers.iterator();
+    var counter: usize = 0;
+    while (it.next()) |kv| {
+        std.mem.copyForwards(u8, buffer[counter..], kv.key_ptr.*);
+        counter += kv.key_ptr.len;
 
-    pub fn addHeader(self: *Response, key: []const u8, value: []const u8) !void {
-        const allocator = self.arena.allocator();
-        const buffer = try allocator.alloc(u8, value.len);
-        std.mem.copyForwards(u8, buffer, value);
-        try self.headers.put(key, buffer);
-    }
+        std.mem.copyForwards(u8, buffer[counter..], delim);
+        counter += delim.len;
 
-    pub fn setContentType(self: *Response, content_type: http.ContentType) !void {
-        try self.addHeader("Content-Type", content_type.toString());
-    }
+        std.mem.copyForwards(u8, buffer[counter..], kv.value_ptr.*);
+        counter += kv.value_ptr.len;
 
-    pub fn setContentLength(self: *Response, size: usize) !void {
-        const allocator = self.arena.allocator();
-        var length: usize = 0;
-        var _size = size;
-        while (_size > 0) {
-            _size /= 10;
-            length += 1;
-        }
-        const buffer = try allocator.alloc(u8, length);
-        _ = std.fmt.formatIntBuf(buffer, size, 10, .lower, .{});
-        try self.addHeader("Content-Length", buffer);
-    }
-
-    pub fn setBody(self: *Response, body: []const u8) !void {
-        const allocator = self.arena.allocator();
-        const buffer = try allocator.alloc(u8, body.len);
-        std.mem.copyForwards(u8, buffer, body);
-        self.body = buffer;
-        try self.setContentLength(@intCast(body.len));
-    }
-
-    pub fn serialize(self: *Response, allocator: std.mem.Allocator) ![]const u8 {
-        const statusLine = try self.statusLineBytes(allocator);
-        defer allocator.free(statusLine);
-
-        const headers = try self.headersBytes(allocator);
-        defer allocator.free(headers);
-
-        const bodyLength = if (self.body) |body| body.len else 0;
-        const buffer = try allocator.alloc(u8, statusLine.len + headers.len + bodyLength);
-        var counter: usize = 0;
-        std.mem.copyForwards(u8, buffer, statusLine);
-        counter += statusLine.len;
-        std.mem.copyForwards(u8, buffer[counter..], headers);
-        counter += headers.len;
-        if (bodyLength > 0) {
-            std.mem.copyForwards(u8, buffer[counter..], self.body.?);
-        }
-
-        return buffer;
-    }
-
-    fn statusLineBytes(self: *Response, allocator: std.mem.Allocator) ![]const u8 {
-        // Calculating size of status line.
-        const space = " ";
-        const crlf = "\r\n";
-        const version = self.version.toString();
-        var statusCode: [3]u8 = undefined;
-        _ = std.fmt.formatIntBuf(&statusCode, self.status.code(), 10, .lower, .{});
-        const status = self.status.toString();
-        const size = version.len + space.len
-            + statusCode.len + space.len
-            + status.len + crlf.len;
-
-        var counter: usize = 0;
-        const buffer = try allocator.alloc(u8, size);
-
-        // Copying HTTP version.
-        std.mem.copyForwards(u8, buffer[counter..], version);
-        counter += version.len;
-        std.mem.copyForwards(u8, buffer[counter..], space);
-        counter += space.len;
-
-        // Copying status code.
-        std.mem.copyForwards(u8, buffer[counter..], &statusCode);
-        counter += statusCode.len;
-        std.mem.copyForwards(u8, buffer[counter..], space);
-        counter += space.len;
-
-        // Copying status.
-        std.mem.copyForwards(u8, buffer[counter..], status);
-        counter += status.len;
-        std.mem.copyForwards(u8, buffer[counter..], crlf);
-
-        return buffer;
-    }
-
-    fn headersBytes(self: *Response, allocator: std.mem.Allocator) ![]const u8 {
-        var it = self.headers.iterator();
-        var size: usize = 0;
-        const delim = ": ";
-        const crlf = "\r\n";
-        // Calculating buffer size.
-        while (it.next()) |kv| {
-             size += kv.key_ptr.len + delim.len + kv.value_ptr.len + crlf.len;
-        }
-        size += crlf.len;
-        const buffer = try allocator.alloc(u8, size);
-
-        // Copying headers to buffer.
-        it = self.headers.iterator();
-        var counter: usize = 0;
-        while (it.next()) |kv| {
-            std.mem.copyForwards(u8, buffer[counter..], kv.key_ptr.*);
-            counter += kv.key_ptr.len;
-
-            std.mem.copyForwards(u8, buffer[counter..], delim);
-            counter += delim.len;
-
-            std.mem.copyForwards(u8, buffer[counter..], kv.value_ptr.*);
-            counter += kv.value_ptr.len;
-
-            std.mem.copyForwards(u8, buffer[counter..], crlf);
-            counter += crlf.len;
-        }
         std.mem.copyForwards(u8, buffer[counter..], crlf);
         counter += crlf.len;
-
-        return buffer;
     }
-};
+    std.mem.copyForwards(u8, buffer[counter..], crlf);
+    counter += crlf.len;
+
+    return buffer;
+}
 
 test "Response struct serialization" {
     var response = try Response.init(std.testing.allocator);
